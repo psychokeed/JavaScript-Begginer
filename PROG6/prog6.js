3b290188a5fb8742c0a815a502addb41e52195e8
psychokeed <bnzuki4@gmail.com>
2024-09-16 11:11:28 +0300

    completed

--- PROG6/prog6.js
+++ PROG6/prog6.js
@@ -1,36 +1,44 @@
-// Random Password Generator
+function generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols) {
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+~`|}{[]:";
 
+    let allowedChars = "";
+    let password = "";
 
-// function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+    allowedChars += includeLowerCase ? lowerCaseChars : "";
+    allowedChars += includeUpperCase ? upperCaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
 
-// }
+    if (length <= 0) {
+        return `(password length must be at least 1)`;
+    }
+    if (allowedChars.length === 0) {
+        return `(password must contain at least 1 set of characters)`;
+    }
 
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
 
+    return password;
+}
 
+function generateAndDisplayPassword() {
+    const passwordLength = 12;
+    const includeLowerCase = true;
+    const includeUpperCase = true;
+    const includeNumbers = true;
+    const includeSymbols = true;
 
+    const password = generatePassword(passwordLength,
+                                      includeLowerCase,
+                                      includeUpperCase,
+                                      includeNumbers,
+                                      includeSymbols);
 
-
-
-
-
-
-
- 
-
-
-
-
-// const passwordLength = 10;
-// const includeLowercase = true;
-// const includeUppercase = true;
-// const includeNumbers = true;
-// const includeSymbols = true;
-
-// const password = generatePassword(passwordLength,
-//                                     includeLowercase,
-//                                     includeUppercase,
-//                                     includeNumbers,
-//                                     includeSymbols);
-
-// console.log(`Generated password: ${password}`);
-                                    
+    document.getElementById("password").value = password; // Set the generated password in the input field
+}
